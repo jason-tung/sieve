9d2a3538d97cfb255b4d506e579eab0094b9e093
Jason <mickeu420@gmail.com>
2018-10-19 11:47:14 -0400

    automated commit and push

---
+++ sieve.c
@@ -0,0 +1,10 @@
+
+
+unsigned int findPrime(int n){
+  if (n == 1) return 2;
+  int size = n * log(n) * 1.15;
+  unsigned int [size] hello;
+  for (unsigned int i = 3; i < size / 2; i+=2){
+    hello[i]
+  }
+}
